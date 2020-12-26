9abf1d707b7eb546e1e0b292ccbf33b28c1011fd
Some-CodingGuy <nopensource@protonmail.com>
2020-12-26 12:45:49 +0100

    First commit

---
+++ src/main/java/be/n/springsecjpa/SpringsecjpaApplication.java
@@ -0,0 +1,13 @@
+package be.n.springsecjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/n/springsecjpa/SpringsecjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.n.springsecjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
